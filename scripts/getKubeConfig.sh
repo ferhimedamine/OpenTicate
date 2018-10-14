658edb4dad8a883b8f1e35f007bfd99957dac731
julienTocci <tocci.julien@etu.unice.fr>
2018-10-14 20:51:01 +0200

    update scripts

--- scripts/getKubeConfig.sh
+++ scripts/getKubeConfig.sh
@@ -1,1 +1,16 @@
-scp -i ~/OpenTicate.pem ubuntu@$1:/home/ubuntu/.kube/config /home/julien/.kube/config
+# Get the kubectl configuration using the Private key you used to create your instances
+# call this script by giving as first arg the ip given at the end of create-cluster
+# and as second argument your linux user to match /home/YOURUSER/.kube/config
+
+scp -i ~/OpenTicate.pem ubuntu@$1:/home/ubuntu/.kube/config /home/$2/.kube/config
+
+sed '4s/^/  /' /home/$2/.kube/config
+sed '5s/^/    /' /home/$2/.kube/config
+sed '6,27s/^/      /' /home/$2/.kube/config
+sed '30s/^/  /' /home/$2/.kube/config
+sed '31s/^/    /' /home/$2/.kube/config
+sed '32s/^/      /' /home/$2/.kube/config
+sed '35s/^/  /' /home/$2/.kube/config
+sed '36s/^/    /' /home/$2/.kube/config
+sed '37s/^/      /' /home/$2/.kube/config
+sed '38s/^/      /' /home/$2/.kube/config
