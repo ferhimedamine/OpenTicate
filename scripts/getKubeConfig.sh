915cd1f27c918bed52f50a4fdb31a717903db8cd
julienTocci <julien.tocci@gmail.com>
2018-10-14 19:12:51 +0000

    working creqte-cluster + setup kubectl

--- scripts/getKubeConfig.sh
+++ scripts/getKubeConfig.sh
@@ -4,13 +4,14 @@
 
 scp -i ~/OpenTicate.pem ubuntu@$1:/home/ubuntu/.kube/config /home/$2/.kube/config
 
-sed '4s/^/  /' /home/$2/.kube/config
-sed '5s/^/    /' /home/$2/.kube/config
-sed '6,27s/^/      /' /home/$2/.kube/config
-sed '30s/^/  /' /home/$2/.kube/config
-sed '31s/^/    /' /home/$2/.kube/config
-sed '32s/^/      /' /home/$2/.kube/config
-sed '35s/^/  /' /home/$2/.kube/config
-sed '36s/^/    /' /home/$2/.kube/config
-sed '37s/^/      /' /home/$2/.kube/config
-sed '38s/^/      /' /home/$2/.kube/config
+sed -i '4s/^/  /' /home/$2/.kube/config
+sed -i '5s/^/    /' /home/$2/.kube/config
+sed -i '6,27s/^/      /' /home/$2/.kube/config
+sed -i '30s/^/  /' /home/$2/.kube/config
+sed -i '31s/^/    /' /home/$2/.kube/config
+sed -i '32s/^/      /' /home/$2/.kube/config
+sed -i '35s/^/  /' /home/$2/.kube/config
+sed -i '36s/^/    /' /home/$2/.kube/config
+sed -i '37s/^/      /' /home/$2/.kube/config
+sed -i '38s/^/      /' /home/$2/.kube/config
+sed -i "s/127.0.0.1\b/$1/g" /home/$2/.kube/config
