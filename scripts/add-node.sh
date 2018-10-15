80db1e9d3960927cb7287707deadb3d2369ebb3d
julienTocci <julien.tocci@gmail.com>
2018-10-15 21:01:22 +0000

    removed unused terraform files

--- scripts/add-node.sh
+++ scripts/add-node.sh
@@ -1,1 +1,1 @@
-terraform  apply -state ../terraform/terraform.tfstate -var-file ../terraform/aws/terraform.tfvars  ../terraform/aws/instance/
+terraform  apply -state ../terraform/terraform.tfstate -var-file ../terraform/aws/terraform.tfvars  ../terraform/aws/
